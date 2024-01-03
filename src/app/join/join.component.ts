38e4941429f4776c4c1ed6c18737e8c3774fa175
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2024-01-03 13:12:26 +0200

    chore: add comments to ngOnInit and remove unused constructor

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-declare var $:any;
+declare var $: any;
 
 @Component({
-  selector: 'app-join',
-  templateUrl: './join.component.html',
-  styleUrls: ['./join.component.scss']
+  selector: "app-join",
+  templateUrl: "./join.component.html",
+  styleUrls: ["./join.component.scss"],
 })
 export class JoinComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
-    $(document).ready(function(){
+    $(document).ready(function () {
       $(".team-carousel").owlCarousel();
-    })
+    });
   }
-
 }
